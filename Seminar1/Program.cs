2d5ae6fd408145e5e877303cded7e99f21e585b2
Kate <kd32107@inbox.ru>
2022-12-19 17:56:57 +0500

    first task

---
+++ Seminar1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+/*Особенностью метода Console.ReadLine() является то,
+что он может считать информацию с консоли только в виде строки.
+По умолчанию платформа .NET предоставляет ряд методов,
+которые позволяют преобразовать различные значения к типам int, double и т.д.
+
+Convert.ToInt32() (преобразует к типу int)
+
+Convert.ToDouble() (преобразует к типу double)
+
+Convert.ToDecimal() (преобразует к типу decimal)
+*/
+
+Console.WriteLine("Напишите два числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(a);
+}
+if (b > a)
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+}
