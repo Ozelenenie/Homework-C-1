80ca8b04b603f45d5833da3f6ea8abf2b72fdbb3
Kate <kd32107@inbox.ru>
2022-12-25 20:05:58 +0500

    задача 23 (семинар 3)

---
+++ Task23/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Напишите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int A = 1;
+
+while (A<N)
+{
+Console.WriteLine(A*A*A);
+A++;
+}
+Console.WriteLine(N*N*N);
