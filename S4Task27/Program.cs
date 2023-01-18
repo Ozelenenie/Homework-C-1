6a44f44465ef1adcd96ae174c71d9fff0e856f18
Kate <kd32107@inbox.ru>
2023-01-19 00:12:36 +0500

    СЕМИНАР 4 задача 27

---
+++ S4Task27/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число : ");
+string number = Console.ReadLine();
+int sum = 0;
+for (int i = 0 ; i < number.Length ; i++)
+{
+    sum = sum + (number[i] - '0');
+}
+Console.WriteLine("Сумма цифр в числе равна " + sum);
