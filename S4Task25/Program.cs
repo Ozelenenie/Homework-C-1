5f327d12a1227c602af49f53eae5cec45edb67d5
Kate <kd32107@inbox.ru>
2023-01-18 23:29:29 +0500

    СЕМИНАР 4  задача 25

---
+++ S4Task25/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B
+
+Console.WriteLine("Введите число A : ");
+string numberA = Console.ReadLine();
+Console.WriteLine("Введите число B : ");
+string numberB = Console.ReadLine();
+int A;
+int B;
+int.TryParse (numberA , out A);
+int.TryParse (numberB , out B);
+
+double pow = Math.Pow(A,B);
+
+Console.WriteLine("A в степени B = " + pow);
