fe44f45ebaefa493d7d9df797a92de4fae8c0bd4
Kate <kd32107@inbox.ru>
2023-01-28 18:32:23 +0500

    СЕМИНАР 5 ЗАДАЧА 36

---
+++ S5Task36/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+
+int[] array = new int[4];
+int sum = 0;
+int num = 0;
+	
+Random rand = new Random();                      //рандомный массив
+for (int x = 0; x < array.Length; x++)
+      {
+          array[x] = rand.Next(100);
+          Console.WriteLine( num + "index " + array[x]);
+          num++;
+      }
+
+for (int i = 1; i < array.Length; i= i + 2)
+{
+sum = array[i] + sum;
+}
+Console.WriteLine("Сумма значений под нечётными индексами = " + sum);
